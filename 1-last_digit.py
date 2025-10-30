e75d1ccf4cbd4ab64c53b2f04c74bba260155204
movlan17 <movlanomerov00@gmail.com>
2025-10-30 12:38:36 +0000

    fdsfsd

---
+++ 1-last_digit.py
@@ -0,0 +1,16 @@
+import random
+
+# Generate a random number between -10000 and 10000
+number = random.randint(-10000, 10000)
+
+# Get the last digit of the number
+last_digit = abs(number) % 10
+
+# Print the result based on the value of the last digit
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+else:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+
