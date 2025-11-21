8da028fbf7216135eb1445497af6de87f7526aeb
movlan17 <movlanomerov00@gmail.com>
2025-11-21 19:57:39 +0400

    fds

---
+++ task_05_dragon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class SwimMixin:
+    """Mixin that adds swimming ability."""
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Mixin that adds flying ability."""
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon class that can both swim and fly."""
+    def roar(self):
+        print("The dragon roars!")
+        
