17523b57d16ca1c378b2c976038ac786b46bcb07
movlan17 <movlanomerov00@gmail.com>
2025-11-30 19:16:32 +0400

    fds

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,8 @@
--- List all shows without a genre
-
-SELECT TV_SHOWS.TITLE, TV_SHOW_GENRES.GENRE_ID
-FROM TV_SHOWS
-LEFT JOIN TV_SHOW_GENRES ON TV_SHOWS.ID = TV_SHOW_GENRES.SHOW_ID
-WHERE TV_SHOW_GENRES.GENRE_ID IS NULL
-ORDER BY TV_SHOWS.TITLE ASC, TV_SHOW_GENRES.GENRE_ID ASC;
-
+-- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
