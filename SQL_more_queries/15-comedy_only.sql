41ae0b08cbefcb9cb0ac2adf2df30ddd33523d3d
movlan17 <movlanomerov00@gmail.com>
2025-11-30 19:19:40 +0400

    fds

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,11 @@
--- List all Comedy shows
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Records are ordered by descending show title.
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
 
-SELECT TV_SHOWS.TITLE
-FROM TV_SHOWS
-JOIN TV_SHOW_GENRES ON TV_SHOWS.ID = TV_SHOW_GENRES.SHOW_ID
-JOIN GENRES ON TV_SHOW_GENRES.GENRE_ID = GENRES.ID
-WHERE GENRES.NAME = 'Comedy'
-ORDER BY TV_SHOWS.TITLE ASC;
-
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
