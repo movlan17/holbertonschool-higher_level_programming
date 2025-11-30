2781043449dd434519da271404e230c0d2c05596
movlan17 <movlanomerov00@gmail.com>
2025-11-30 18:21:04 +0400

    fsd

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,11 @@
+-- 1-create_user.sql
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
+
