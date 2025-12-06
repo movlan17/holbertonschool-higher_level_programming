e1568461218435c09994390af10307af477f3efa
movlan17 <movlanomerov00@gmail.com>
2025-12-06 11:02:47 +0400

    fds

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table where name matches the argument,
-safe from SQL injection.
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
 """
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
 
-    cur = conn.cursor()
-
-    # Safe query preventing SQL injection
-    cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-        (sys.argv[4],)
-    )
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    conn.close()
-
+    for state in states:
+        print(state)
