2472cbeb8cc90149b4c927bc779bd1fc1cefd81d
movlan17 <movlanomerov00@gmail.com>
2025-12-25 17:16:12 +0400

    cghj

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,8 @@ const args = process.argv.slice(2).map(Number);
 if (args.length < 2) {
   console.log(0);
 } else {
-  let max = Math.max(...args);
-  let filtered = args.filter(num => num !== max);
-  let secondMax = Math.max(...filtered);
+  const max = Math.max(...args);
+  const filtered = args.filter(num => num !== max);
+  const secondMax = Math.max(...filtered);
   console.log(secondMax);
 }
-
