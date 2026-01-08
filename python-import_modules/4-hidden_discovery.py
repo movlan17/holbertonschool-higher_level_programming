b4da02e254fb791372ca986c33830bd60f130c2b
movlan17 <movlanomerov00@gmail.com>
2026-01-08 18:23:53 +0400

    fds

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
-
     for name in sorted(dir(hidden_4)):
-        if not name.startswith("__"):
+        if name[:2] != "__":
             print(name)
