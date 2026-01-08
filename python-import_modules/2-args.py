4cf030f3a08db19ca38a5e04ece23062e6c6aea8
movlan17 <movlanomerov00@gmail.com>
2026-01-08 18:17:58 +0400

    fds

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+    argc = len(sys.argv) - 1
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(i))
+        print("{} arguments:".format(argc))
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
